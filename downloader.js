f6fc9dbdeaac971a6c3a1f0bdc90867070c9d1ff
nuranai <bajanga07@gmail.com>
2022-10-09 15:58:40 +0700

    init

--- downloader.js
+++ downloader.js
@@ -7,21 +7,11 @@ downloadButton.appendChild(text);
 info.parentElement.appendChild(downloadButton);
 
 downloadButton.addEventListener("click", () => {
-  // console.log(window.location);
   window.location.href= `http://localhost:4000/download?URL=${window.location.href}`
-  // console.log(window.ytInitialData.currentVideoEndpoint.watchEndpoint.watchEndpointSupportedOnesieConfig.html5PlaybackOnesieConfig.commonConfig.url);
 });
 
 let data;
 
-// const intervalId = setInterval(()=> {
-//   console.log('triggered');
-//   console.log(window.ytInitialData);
-//   if (window["ytInitialData"]) {
-//     console.log('horray');
-//     clearInterval(intervalId);
-//   }
-// }, 500);
 console.log(document.querySelectorAll('script'))
 
 window.addEventListener('load', () => console.log(window?.ytInitialData));
